f1f8d5cc79d9f96fa08909658ca0bc0beccdd836
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-12 12:26:50 +0800

    具体项目流程审批移入

---
+++ lichkin-projects-attendance-employee-ui/src/main/resources/META-INF/resources/res/js/employee/activiti/submitForm/LEAVE/i18n/zh_CN.js
@@ -0,0 +1,21 @@
+$.LKExtendI18N({
+  'LEAVE' : '请假',
+
+  'userName' : '姓名',
+  'deptName' : '部门',
+  'startDate' : '开始日期',
+  'endDate' : '截止日期',
+  'reason' : '请假原因',
+
+  'confirmSave' : '保存后将不能修改,确认保存？',
+  'Save successfully' : '保存成功',
+
+  'submit' : '提交',
+  'Submit successfully' : '提交成功',
+
+  'Cancel application' : '取消申请',
+  'Cancel successfully' : '取消成功',
+
+  'comment' : '备注'
+
+});
