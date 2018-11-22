26f4bb4eeffef1c0fd55aef079d21ad0e58ad0c8
China-zhuangxuxin <zhuangxuxin@hotmail.com>
2018-11-23 05:22:38 +0800

    消除代码警告

--- lichkin-projects-attendance-admin/src/main/java/com/lichkin/application/apis/api40001/I/n00/S.java
+++ lichkin-projects-attendance-admin/src/main/java/com/lichkin/application/apis/api40001/I/n00/S.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lichkin.application.services.bus.impl.SysCompScheduleConfigBusService;
-import com.lichkin.framework.defines.enums.impl.LKUsingStatusEnum;
 import com.lichkin.framework.defines.enums.LKCodeEnum;
 import com.lichkin.springframework.entities.impl.SysCompScheduleConfigEntity;
 import com.lichkin.springframework.services.LKApiBusInsertService;
